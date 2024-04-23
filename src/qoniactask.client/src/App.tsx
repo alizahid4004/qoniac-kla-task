8099838f87bf8063d6f757334bf3629bf111973e
Ali Zahid <alizahid4004@outlook.com>
2024-04-23 21:30:58 +0800

    fix: cosmetic fixes

--- src/qoniactask.client/src/App.tsx
+++ src/qoniactask.client/src/App.tsx
@@ -32,9 +32,15 @@ const App = () => {
       return;
     }
 
-    // value is over limit
     if (Number(_value) > max) {
       setErrorMessage(`Max: ${prefix}${max}`);
+      setClassName("border-red-500");
+      setValue(_value);
+      return;
+    }
+
+    if (Number(_value) < min) {
+      setErrorMessage(`Min: ${prefix}${max}`);
       setClassName("border-red-500");
       setValue(_value);
       return;
@@ -58,7 +64,7 @@ const App = () => {
   </div>
 
   <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-    <form className="space-y-6" action="#" method="POST">
+    <form className="space-y-6">
       <div>
         <div className="flex items-center justify-between">
           <label
@@ -72,14 +78,14 @@ const App = () => {
           <CurrencyInput
             id="currency-input"
             name="currency-input"
-            className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ${className}`}
+            className={`block w-full rounded-md border-0 py-2 px-2 size-12 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-base sm:leading-6 ${className}`}
             value={value}
             onValueChange={handleOnValueChange}
             placeholder="Please enter a value"
             prefix={prefix}
             step={1}
             allowNegativeValue={false}
-            fixedDecimalLength={2}
+            fixedDecimalLength={fixedDecimalLength}
             decimalSeparator={decimalSeparator}
             groupSeparator={groupSeparator}
             disableAbbreviations={true}
@@ -90,19 +96,15 @@ const App = () => {
 
       <div>
         <button
-          type="submit"
-          className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          type="button"
+          className="flex w-full justify-center size-12 rounded-md bg-indigo-600 px-3 py-1.5 text-base font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
         >
-          Convert
+          Describe
         </button>
       </div>
     </form>
-    <p className="mt-10 text-center text-sm text-gray-500">
-      Made by Ali Zahid for Qoniac/KLA task 😊
-    </p>
   </div>
 </div>
-
   );
 };
 
