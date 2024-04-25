5d2646b7ddb363247da4d192699521694805cfe2
Ali Zahid <alizahid4004@outlook.com>
2024-04-25 12:54:35 +0800

    test: added additional parsing test cases

--- test/QoniacTask.Api.Test/CurrencyParserTests.cs
+++ test/QoniacTask.Api.Test/CurrencyParserTests.cs
@@ -4,6 +4,22 @@ namespace QoniacTask.Api.Tests
 {
     public class CurrencyParserTests
     {
+        [Theory]
+        [InlineData("-0", 0L, null)]
+        [InlineData("0", 0L, null)]
+        [InlineData("0,01", 0L, 1U)]
+        [InlineData("10,5", 10L, 50U)]
+        [InlineData("999 999 999", 999_999_999L, null)]
+        [InlineData("999 999 999,99", 999_999_999L, 99U)]
+        public void Parse_ShouldParseFormattedAmount(
+            string formattedAmount, long whole, uint? change)
+        {
+            var currency = CurrencyParser.Parse(formattedAmount);
+
+            Assert.Equal(whole, currency.Whole);
+            Assert.Equal(change, currency.Change);
+        }
+
         [Fact]
         public void Parse_ShouldThrowArgumentException_WhenEmptyStringIsPassed()
         {
@@ -29,11 +45,33 @@ namespace QoniacTask.Api.Tests
         }
 
         [Fact]
-        public void Parse_ShouldThrowInvalidOperationException_WhenInvalidOptionsArePassed()
+        public void Parse_ShouldThrowInvalidOperationException_WhenGroupAndDecimalSeparatorsAreSame()
         {
             Action result = () => CurrencyParser.Parse(
                 "123",
                 new CurrencyParserOptions { DecimalSeparator = ' ', GroupSeparator = ' ' });
+
+            Assert.Throws<InvalidOperationException>(result);
+        }
+
+        [Theory]
+        [InlineData("-26")]
+        [InlineData("101")]
+        public void Parse_ShouldParseFormattedAmount_WhenXSettingsAreProvided(
+            string formattedAmount)
+        {
+            Action result = () => CurrencyParser.Parse(
+                formattedAmount, new CurrencyParserOptions { WholeMinimum = -25, WholeMaximum = 100 });
+
+            Assert.Throws<FormatException>(result);
+        }
+
+        [Fact]
+        public void Parse_ShouldThrowInvalidOperationException_WhenMinimumIsLargerThanMaximum()
+        {
+            Action result = () => CurrencyParser.Parse(
+                "123",
+                new CurrencyParserOptions { WholeMinimum = 2, WholeMaximum = 1 });
 
             Assert.Throws<InvalidOperationException>(result);
         }
