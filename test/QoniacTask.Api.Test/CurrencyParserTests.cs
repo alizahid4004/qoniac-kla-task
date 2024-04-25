331f2fb23df686492678b89155f1705e9941d6ac
Ali Zahid <alizahid4004@outlook.com>
2024-04-25 11:42:00 +0800

    test: added parsing exception related tests

---
+++ test/QoniacTask.Api.Test/CurrencyParserTests.cs
@@ -0,0 +1,41 @@
+﻿using QoniacTask.Api.Services;
+
+namespace QoniacTask.Api.Tests
+{
+    public class CurrencyParserTests
+    {
+        [Fact]
+        public void Parse_ShouldThrowArgumentException_WhenEmptyStringIsPassed()
+        {
+            Action result = () => CurrencyParser.Parse(string.Empty);
+
+            Assert.Throws<ArgumentException>(result);
+        }
+
+        [Fact]
+        public void Parse_ShouldThrowFormatException_WhenStringContainsAlphabets()
+        {
+            Action result = () => CurrencyParser.Parse("123 456 X");
+
+            Assert.Throws<FormatException>(result);
+        }
+
+        [Fact]
+        public void Parse_ShouldThrowFormatException_WhenDecimalSeparatorRepeats()
+        {
+            Action result = () => CurrencyParser.Parse("123 456,12,34");
+
+            Assert.Throws<FormatException>(result);
+        }
+
+        [Fact]
+        public void Parse_ShouldThrowInvalidOperationException_WhenInvalidOptionsArePassed()
+        {
+            Action result = () => CurrencyParser.Parse(
+                "123",
+                new CurrencyParserOptions { DecimalSeparator = ' ', GroupSeparator = ' ' });
+
+            Assert.Throws<InvalidOperationException>(result);
+        }
+    }
+}
