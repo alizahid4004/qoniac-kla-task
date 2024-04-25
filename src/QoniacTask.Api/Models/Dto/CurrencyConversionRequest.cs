a8f848c3cffff134dc3188320d40ed4470844aee
Ali Zahid <alizahid4004@outlook.com>
2024-04-25 16:12:31 +0800

    fix: added missing bind required

--- src/QoniacTask.Api/Models/Dto/CurrencyConversionRequest.cs
+++ src/QoniacTask.Api/Models/Dto/CurrencyConversionRequest.cs
@@ -1,15 +1,18 @@
-﻿using QoniacTask.Api.Validation;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using QoniacTask.Api.Validation;
 using System.ComponentModel.DataAnnotations;
 
 namespace QoniacTask.Api.Models
 {
     public record CurrencyConversionRequest(
         [Required]
+        [BindRequired]
         [DecimalRangeValidation(0, 999999999, 0, 99)]
         [Display(Name = "currency amount")] decimal Amount);
 
     public record CurrencyParseAndConvertRequest(
         [Required]
+        [BindRequired]
         [MinLength(1)]
         [Display(Name = "formatted currency amount")]
         string Amount);
